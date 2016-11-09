95516b9dacdd270e8018c55ecd91a6ef2bafc54f
Kevin Chung <kvnchung92@gmail.com>
2016-11-09 14:36:55 -0600

    sync

---
+++ app/src/main/java/mobi/airberlin/PassengerModel.java
@@ -0,0 +1,13 @@
+package mobi.airberlin;
+
+/**
+ * Created by Kevin on 11/9/2016.
+ *
+ *
+ * model for passenger, needs id, maybe credit card, not sure about the rest
+ */
+
+public class PassengerModel {
+    private String passengerID, creditCardID;
+
+}
